2ec9ad9e91a47852d5e9150f3a6bfc600716db11
Atul Anilkumar Kumbhar <atulkumbhar.edu@gmail.com>
2020-02-05 23:04:46 +0530

    login_session_mysql

---
+++ login_session_mysql/session.php
@@ -0,0 +1,19 @@
+
+<?php
+//Session.php will verify the session, if there is no session it will redirect to login page.
+   include('config.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['username'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+      die();
+   }
+?>
---
+++ login_session_mysql/welcome.php
@@ -0,0 +1,16 @@
+<?php
+//After successful login, it will display welcome page.
+   include('session.php');
+?>
+<html">
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome <?php echo $login_session; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
